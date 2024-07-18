aa1456f127688f758febfef85c1e438db8366a82
codewithwest <jonaslekgau@gmail.com>
2024-07-18 20:27:28 +0200

    removing git cache

--- src/providers/image_provider.js
+++
@@ -1,6 +0,0 @@
-import main_logo from "../assets/0.jpg";
-import iaaf_logo from "../assets/logos/iaaf.png";
-import olympics_logo from "../assets/logos/olympic.jpg";
-import timetronics_logo from "../assets/logos/timetronics.webp";
-
-export { main_logo, iaaf_logo, olympics_logo, timetronics_logo }
